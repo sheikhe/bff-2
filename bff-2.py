53c12f1989753163f19b516be874c8d3a73fb863
Romi Afrizal <81342754+Mark-Zuck@users.noreply.github.com>
2022-01-28 20:07:53 +0700

    Update bff-2.py

--- bff-2.py
+++ bff-2.py
@@ -3,6 +3,6 @@ import os
 if __name__=='__main__':
     try:
         os.system('git pull')
-        __import__("crack").menu()
+        __import__("cracks").menu()
     except Exception as e:
         exit(str(e))
