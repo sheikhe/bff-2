eaf21a8dc2bed06ed591667f5ee11d351eb53563
Romi Afrizal <81342754+Mark-Zuck@users.noreply.github.com>
2022-02-04 21:43:09 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,8 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-try:
-        os.system("rm -rf *.so")
-except:
-        pass
 
-ext = Extension(
-        "cracks",sources=["cracks.c"]
-)
+ext = Extension("cracks",sources=["cracks.c"])
 
 setup(
 	ext_modules=[ext],
